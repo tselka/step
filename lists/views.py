6822060087af15d4ac302a19861ecef8028802ea
Stepan Chernousov <stepa1818@yandex.ru>
2024-08-18 16:19:38 +0500

    ФТ теперь проверяет возможность ввести элемент списка

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def home_page(request):
     """домашняя страница"""
-    return HttpResponse('<html><title>иди нахуй</title></html>')
+    return HttpResponse('<html><title>To-Do lists</title></html>')
