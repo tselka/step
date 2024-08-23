54fc663e42c398ee39d5746d01f5513c8f0771e8
Stepan Chernousov <stepa1818@yandex.ru>
2024-08-23 14:01:32 +0500

    Сайт обрабатывает один элемент списка

--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,9 @@ class HomePageTest(TestCase):
         """тест: исползуется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
